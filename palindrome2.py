b968ae24f4c84403c31c9760b1965e377c6b5b61
CarlangasDeveloper <cf9220543@gmail.com>
2023-09-08 19:38:36 -0500

    "Programa en python que verifica si un número ingresado por el usuario es un palíndromo o no."

---
+++ palindrome2.py
@@ -0,0 +1,10 @@
+numero_palindromo = int(input("Ingrese un número: "))
+
+# Se convierte el número ingresado en un String
+num_str = str(numero_palindromo)
+    
+# Comparamos el String con el String invertido
+if num_str == num_str[::-1]:
+    print("El número ingresado es palindromo")
+else:
+    print("El número ingresado NO es palindromo")
